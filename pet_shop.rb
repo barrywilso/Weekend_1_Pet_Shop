ff984a83b0e44a77e4d91479d796ba204045d603
barrywilso <barrywilso@googlemail.com>
2019-07-19 16:37:54 +0100

    first commit

---
+++ pet_shop.rb
@@ -0,0 +1,13 @@
+def pet_shop_name(name)
+  return name[:name]
+end
+
+def total_cash(sum)
+  return sum[:admin][:total_cash]
+end
+
+def add_or_remove_cash(sum)
+
+  
+  return sum[:admin][:total_cash]
+end
